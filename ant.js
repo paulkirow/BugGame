24455b213651ad378cc8e2b834515c260306c86c
paulkirow <paul.kirow@mail.utoronto.ca>
2015-06-08 16:41:28 -0400

    Completed more functionallity, however I dont expect most of it works properly yet - figuring it out as I go.

--- ant.js
+++ ant.js
@@ -6,8 +6,10 @@
     FOOD_HIGHT = 20,
     BUG_WIDTH = 10,
     BUG_HIGHT = 40,
-    SPAWN_MIN = 10,
-    SPAWN_MAX = 390,
+    X_SPAWN_MIN = 10,
+    X_SPAWN_MAX = 390,
+    Y_SPAWN_MIN = 100,
+    Y_SPAWN_MAX = 500,
     SPAWN_TIME_MIN = 1,
     SPAWN_TIME_MAX = 3,
     BLACK_SPEED = 150,
@@ -19,6 +21,7 @@
     ORANGE_SPEED = 60,
     ORANGE_SCORE = 60,
     ORANGE_PROBABILITY = 60,
+    FOOD_COUNT = 5,
     LEVEL1_BUFF = 1.0,
     LEVEL2_BUFF = 0.75,
     FADE_TIME = 2,
@@ -31,60 +34,102 @@
     gamestate,
     score,
     highscore,
-    timer,
-    foodlist,
-    food = {
-        x: null,
-        y: null,
-        eaten: null,
-        init: function(x, y) {
-            this.x = x;
-            this.y = y;
-            this.eaten = false;
+    timer = {
+        time: null,
+        paused: true,
+        init: function() {
+            this.time = TIMER_START;
         },
-        eat: function() {
-            this.eaten = true;
+        pause: function() {
+            this.paused = true;
+        },
+        start: function() {
+            this.paused = false;
         }
     },
-    ant;
+    food = {
+        list: new Array(),
+        create: function(x, y) {
+            this.list.push({ posx: x, posy: y, eaten: false });
+        },
+        eat: function() {
+            this.list.eaten = true;
+        }
+    },
+    ant = {
+        
+    };
 
+/**
+ * Sets a single food onto a random position within the bounds
+ * @returns {food} 
+ */
 function setFood() {
-    
+    // choose a random position
+    var randx = Math.round(Math.random() * (X_SPAWN_MAX - X_SPAWN_MIN) + X_SPAWN_MIN - FOOD_WIDTH);
+    var randy = Math.round(Math.random() * (Y_SPAWN_MAX - Y_SPAWN_MIN) + Y_SPAWN_MIN - FOOD_HIGHT);
+    return food.create(randx, randy);
+}
+
+/**
+ * Calculates the distance squared between a given ant and a given food
+ * @param {int} antx 
+ * @param {int} anty 
+ * @param {int} foodx 
+ * @param {int} foody 
+ * @returns {int} 
+ */
+function getDistance(antx, anty, foodx, foody) {
+    return Math.pow(antx - foodx, 2) + Math.pow(anty - foody, 2);
 }
 
 function init() {
     score = 0;
-    // retrive high score
-    if (Document.localstorage) {
-        highscore = parseInt(localStorage["highscore"]||'0', 10);
+    localStorage["highscore"] = 20;
+    // retrive high score from browser storage if it exists
+    if (document.localstorage) {
+        highscore = parseInt(localStorage["highscore"] || '0', 10);
+        alert(highscore);
     } else {
-        // local storage unavailable
+        // local storage unavailable, set high score to 0
         highscore = 0;
     }
+
+    // Initialize the food objects
+    for (i = 0; i < FOOD_COUNT; i++) {
+        foodlist.push(setFood());
+        alert(foodlist[i].eaten);
+    }
+
 }
 
 function play() { }
 
+/**
+ * Main function for running the game
+ */
 function main() {
     // Create the canvas element
-    canvas = Document.createElement("canvas");
+    canvas = document.createElement("canvas");
     canvas.WIDTH = WIDTH;
     canvas.HEIGHT = HEIGHT;
+
     ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
     // add the canvas to the body of the html
-    Document.body.appendChild(canvas);
+    document.body.appendChild(canvas);
 
     // tracks the mouse activity
-    Document.addEventListener("mousedown", function(evt) {
+    document.addEventListener("mousedown", function(evt) {
         clickstate["X"] = evt.clientX;
         clickstate["Y"] = evt.clientX;
     });
 
-    Document.addEventListener("mouseup", function (evt) {
+    document.addEventListener("mouseup", function (evt) {
         delete clickstate["X"];
         delete clickstate["Y"];
     });
-
+    alert("test1");
     // initialize the game
     init();
     // start the game
